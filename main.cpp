b329e53280925871463ca6deea38943b064e0428
Utsab Sen <19177728+UtsabSen@users.noreply.github.com>
2019-04-20 18:07:48 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 #include<iostream>
 using namespace std;
 
-int total_process, TQ = 2;
+int total_process, total_time = 0, total_queue, TQ = 2;
 
 struct processes{
     int AT, BT, queue, CT = 0, TAT = 0, WT = 0, fixed_BT;
 }*process;
+
+int max_queue(){
+    int max = 0;
+    for (int i = 0; i < total_process; i++) {
+        if (process[i].queue > max){
+            max = process[i].queue;
+        }
+    }
+    return max;
+}
+
+
 
 int main(){
     cout << "Enter total process: ";
@@ -29,4 +41,6 @@ int main(){
         cout << "  P"<<(i+1) << "\t\t" << process[i].AT << "\t\t" << process[i].fixed_BT << "\t  " << process[i].queue <<
              "\t\t" << process[i].CT << "\t\t\t" << process[i].TAT << "\t\t\t" << process[i].WT << endl;
     }
+
+    total_queue = max_queue();
 }
