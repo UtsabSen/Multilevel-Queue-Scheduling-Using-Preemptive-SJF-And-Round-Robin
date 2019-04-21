d316f1b60d5c2a6a5fd6b249be4e6ce892ad7f93
Utsab Sen <19177728+UtsabSen@users.noreply.github.com>
2019-04-21 17:33:56 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<iostream>
+#include<windows.h>
 using namespace std;
 
 int total_process, total_time = 0, total_queue, TQ = 2;
@@ -18,7 +19,96 @@ int max_queue(){
     return max;
 }
 
+int high_priority_queue(){
+    for (int i = 0; i < total_queue; i++) {
+        for (int j = 0; j < total_process; j++) {
+            if((process[j].queue == i && process[j].BT > 0) && (total_time >= process[j].AT)){
+                return process[j].queue;
+            }
+        }
+    }
+}
 
+int least_BT(int queue){
+    int small_BT = 0, flag = 0;
+    for (int i = 0; i < total_process; i++) {
+        if (process[i].BT > 0 && process[i].queue == queue && total_time >= process[i].AT){
+            small_BT = process[i].BT;
+            flag = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < total_process; i++) {
+        if (small_BT > process[i].BT && process[i].BT > 0 && process[i].queue == queue && total_time >= process[i].AT){
+            small_BT = process[i].BT;
+            flag = i;
+        }
+    }
+    return flag;
+}
+
+int complete_process(){
+    for (int i = 0; i < total_process; i++){
+        if (process[i].BT > 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int arrive_process(){
+    for (int i = 0; i < total_process; i++) {
+        if(process[i].BT > 0 && total_time >= process[i].AT){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void process_execution(){
+
+    int t[50] = {0};
+    int c;
+
+    int checkpoint = 1;
+    while (!complete_process()){
+        if (!arrive_process()){
+            int p_id = least_BT(high_priority_queue());
+            for (int i = 0; i < TQ; i++) {
+                process[p_id].BT--;
+                total_time++;
+                if(checkpoint == 1){
+                    c = process[p_id].AT;
+                    checkpoint = 0;
+                    cout << "Start: " << c << "\t";
+                }
+                c++;
+                cout << "P" << (p_id+1) <<" -> "<< c << " \t";
+                t[i] = c;
+                Sleep(200);
+
+                for (int j = 0; j < total_process; j++) {
+                    if(j != p_id && total_time > process[j].AT && process[j].BT > 0){
+                        process[j].WT++;
+                    }
+                }
+
+                if (process[p_id].BT == 0){
+                    process[p_id].CT = total_time;
+                    process[p_id].TAT = process[p_id].CT - process[p_id].AT;
+                    break;
+                }
+
+                if (high_priority_queue() < process[p_id].queue){
+                    break;
+                }
+            }
+        } else{
+            ++total_time;
+        }
+    }
+}
 
 int main(){
     cout << "Enter total process: ";
@@ -36,11 +126,16 @@ int main(){
         process[i].fixed_BT = process[i].BT;
     }
 
+
+    total_queue = max_queue();
+
+    cout << "Gantt Chart\n";
+    process_execution();
+
     cout << "\n\nProcess\t Arrival Time\t Burst Time\t Queue\t Completion Time\t Turn Around Time\t Waiting Time\t\n";
     for (int i = 0; i < total_process; i++) {
         cout << "  P"<<(i+1) << "\t\t" << process[i].AT << "\t\t" << process[i].fixed_BT << "\t  " << process[i].queue <<
              "\t\t" << process[i].CT << "\t\t\t" << process[i].TAT << "\t\t\t" << process[i].WT << endl;
     }
 
-    total_queue = max_queue();
 }
